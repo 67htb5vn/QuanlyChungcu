958d40516520a6951eab13ec2fa2ca4e8023086e
meomeogaugau <maivunguyetanh@gmail.com>
2025-05-24 20:35:28 +0700

    them_HoaDon

---
+++ QuanlyChungcu/FormHoaDonDV.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace QuanlyChungcu
+{
+    partial class FormHoaDonDV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonKhoiTao = new Button();
+            buttonXoa = new Button();
+            buttonSua = new Button();
+            buttonThem = new Button();
+            buttonTimKiem = new Button();
+            groupBoxHoaDonTPhong2 = new GroupBox();
+            datagridViewHoaDonDV = new DataGridView();
+            groupBoxHoaDonDV1 = new GroupBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            groupBoxHoaDonTPhong2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)datagridViewHoaDonDV).BeginInit();
+            groupBoxHoaDonDV1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonKhoiTao
+            // 
+            buttonKhoiTao.Location = new Point(536, 230);
+            buttonKhoiTao.Name = "buttonKhoiTao";
+            buttonKhoiTao.Size = new Size(94, 29);
+            buttonKhoiTao.TabIndex = 20;
+            buttonKhoiTao.Text = "Khởi tạo";
+            buttonKhoiTao.UseVisualStyleBackColor = true;
+            // 
+            // buttonXoa
+            // 
+            buttonXoa.Location = new Point(404, 230);
+            buttonXoa.Name = "buttonXoa";
+            buttonXoa.Size = new Size(94, 29);
+            buttonXoa.TabIndex = 19;
+            buttonXoa.Text = "Xóa";
+            buttonXoa.UseVisualStyleBackColor = true;
+            // 
+            // buttonSua
+            // 
+            buttonSua.Location = new Point(239, 230);
+            buttonSua.Name = "buttonSua";
+            buttonSua.Size = new Size(94, 29);
+            buttonSua.TabIndex = 18;
+            buttonSua.Text = "Sửa";
+            buttonSua.UseVisualStyleBackColor = true;
+            // 
+            // buttonThem
+            // 
+            buttonThem.Location = new Point(63, 230);
+            buttonThem.Name = "buttonThem";
+            buttonThem.Size = new Size(94, 29);
+            buttonThem.TabIndex = 17;
+            buttonThem.Text = "Thêm";
+            buttonThem.UseVisualStyleBackColor = true;
+            // 
+            // buttonTimKiem
+            // 
+            buttonTimKiem.Location = new Point(622, 77);
+            buttonTimKiem.Name = "buttonTimKiem";
+            buttonTimKiem.Size = new Size(94, 29);
+            buttonTimKiem.TabIndex = 16;
+            buttonTimKiem.Text = "Tìm kiếm";
+            buttonTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxHoaDonTPhong2
+            // 
+            groupBoxHoaDonTPhong2.Controls.Add(datagridViewHoaDonDV);
+            groupBoxHoaDonTPhong2.Location = new Point(-2, 283);
+            groupBoxHoaDonTPhong2.Name = "groupBoxHoaDonTPhong2";
+            groupBoxHoaDonTPhong2.Size = new Size(727, 231);
+            groupBoxHoaDonTPhong2.TabIndex = 15;
+            groupBoxHoaDonTPhong2.TabStop = false;
+            groupBoxHoaDonTPhong2.Text = "groupBoxHoaDonTPhong2";
+            // 
+            // datagridViewHoaDonDV
+            // 
+            datagridViewHoaDonDV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            datagridViewHoaDonDV.Location = new Point(50, 59);
+            datagridViewHoaDonDV.Name = "datagridViewHoaDonDV";
+            datagridViewHoaDonDV.RowHeadersWidth = 51;
+            datagridViewHoaDonDV.Size = new Size(582, 151);
+            datagridViewHoaDonDV.TabIndex = 0;
+            datagridViewHoaDonDV.CellContentClick += datagridViewHoaDonDV_CellContentClick;
+            // 
+            // groupBoxHoaDonDV1
+            // 
+            groupBoxHoaDonDV1.Controls.Add(label6);
+            groupBoxHoaDonDV1.Controls.Add(label5);
+            groupBoxHoaDonDV1.Controls.Add(label4);
+            groupBoxHoaDonDV1.Controls.Add(label3);
+            groupBoxHoaDonDV1.Controls.Add(label2);
+            groupBoxHoaDonDV1.Controls.Add(label1);
+            groupBoxHoaDonDV1.Controls.Add(textBox6);
+            groupBoxHoaDonDV1.Controls.Add(textBox5);
+            groupBoxHoaDonDV1.Controls.Add(textBox4);
+            groupBoxHoaDonDV1.Controls.Add(textBox3);
+            groupBoxHoaDonDV1.Controls.Add(buttonTimKiem);
+            groupBoxHoaDonDV1.Controls.Add(textBox2);
+            groupBoxHoaDonDV1.Controls.Add(textBox1);
+            groupBoxHoaDonDV1.Location = new Point(-2, 27);
+            groupBoxHoaDonDV1.Name = "groupBoxHoaDonDV1";
+            groupBoxHoaDonDV1.Size = new Size(727, 179);
+            groupBoxHoaDonDV1.TabIndex = 14;
+            groupBoxHoaDonDV1.TabStop = false;
+            groupBoxHoaDonDV1.Text = "groupBoxHoaDonDV1";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(479, 121);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(89, 27);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(189, 125);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(94, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(479, 84);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(89, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(189, 80);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(94, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(479, 34);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(89, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(189, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(94, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 20);
+            label1.TabIndex = 17;
+            label1.Text = "Mã hóa đơn";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(344, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 20);
+            label2.TabIndex = 18;
+            label2.Text = "Tháng sử dụng";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 20);
+            label3.TabIndex = 19;
+            label3.Text = "Trạng thái thanh toán";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(332, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(119, 20);
+            label4.TabIndex = 20;
+            label4.Text = "Ngày thanh toán";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(22, 128);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 20);
+            label5.TabIndex = 21;
+            label5.Text = "Mã hợp đồng";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(350, 128);
+            label6.Name = "label6";
+            label6.Size = new Size(101, 20);
+            label6.TabIndex = 22;
+            label6.Text = "Mã doanh thu";
+            // 
+            // FormHoaDonDV
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(726, 520);
+            Controls.Add(buttonKhoiTao);
+            Controls.Add(buttonXoa);
+            Controls.Add(buttonSua);
+            Controls.Add(buttonThem);
+            Controls.Add(groupBoxHoaDonTPhong2);
+            Controls.Add(groupBoxHoaDonDV1);
+            Name = "FormHoaDonDV";
+            Text = "FormHoaDonDV";
+            Load += FormHoaDonDV_Load;
+            groupBoxHoaDonTPhong2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)datagridViewHoaDonDV).EndInit();
+            groupBoxHoaDonDV1.ResumeLayout(false);
+            groupBoxHoaDonDV1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonKhoiTao;
+        private Button buttonXoa;
+        private Button buttonSua;
+        private Button buttonThem;
+        private Button buttonTimKiem;
+        private GroupBox groupBoxHoaDonTPhong2;
+        private DataGridView datagridViewHoaDonDV;
+        private GroupBox groupBoxHoaDonDV1;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ QuanlyChungcu/FormHoaDonDV.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyChungcu
+{
+    public partial class FormHoaDonDV : Form
+    {
+        public FormHoaDonDV()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHoaDonDV_Load(object sender, EventArgs e)
+        {
+            loadHoaDonDV();
+        }
+
+        void loadHoaDonDV()
+        {
+            datagridViewHoaDonDV.DataSource = DataConnect.GetData("SELECT * FROM HoaDonDV");
+            datagridViewHoaDonDV.AllowUserToAddRows = false;
+            datagridViewHoaDonDV.RowHeadersVisible = false;
+        }
+
+        private void datagridViewHoaDonDV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = datagridViewHoaDonDV.CurrentRow.Index;
+            textBox1.Text=datagridViewHoaDonDV.Rows[i].Cells[0].Value.ToString();
+            textBox2.Text = datagridViewHoaDonDV.Rows[i].Cells[1].Value.ToString();
+            textBox3.Text = datagridViewHoaDonDV.Rows[i].Cells[7].Value.ToString();
+            textBox4.Text = datagridViewHoaDonDV.Rows[i].Cells[8].Value.ToString();
+            textBox5.Text = datagridViewHoaDonDV.Rows[i].Cells[10].Value.ToString();
+        }
+    }
+}
